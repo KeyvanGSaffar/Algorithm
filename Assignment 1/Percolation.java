856b3ffdca3a044ace3a18c7d3156c1d8329d904
Keyvan GSaffar <gharouni@uaberta.ca>
2016-11-06 12:15:10 -0700

    Passed 90%- backwash not solved

--- Assignment 1/Percolation.java
+++ Assignment 1/Percolation.java
@@ -1,76 +1,120 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-//import java.lang.IndexOutOfBoundsException;
 
 public class Percolation {
-	private int sideNumber;
-	private int[] siteStatus;
-	private WeightedQuickUnionUF grid;
+  private int sideNumber;
+  private int[][] siteStatus;
+  private WeightedQuickUnionUF grid;
 
-	public Percolation(int n) { // create n-by-n grid, with all sites blocked
-		grid = new WeightedQuickUnionUF(n * n + 2);
-		siteStatus = new int[n * n];
-		for (int i = 0; i < n * n; i++) {
-			siteStatus[i] = 0; // zero means that the site is blocked (full)
-		}
-		for (int i = 1; i <= n; i++) {
-			grid.union(0, i);
-			grid.union(n * n + 1, n * n + 1 - (i));
-		}
-		this.sideNumber = n;
-	}
+  public Percolation(int n) { // create n-by-n grid, with all sites blocked
+    grid = new WeightedQuickUnionUF(n * n + 2);
+    siteStatus = new int[n + 2][n + 2];
+    for (int i = 0; i < n + 2; i++) {
+      for (int j = 0; j < n + 2; j++) {
+        siteStatus[i][j] = 0; // zero means that the site is blocked
+        // (full)
+      }
+    }
+    // for (int i = 1; i <= n; i++) {
+    // grid.union(0, i);
+    // grid.union(n * n + 1, n * n + 1 - (i));
+    // }
+    this.sideNumber = n;
+  }
 
-	private void valid(int row, int col) {
-		if (row < 1 || row > this.sideNumber || col < 1 || col > this.sideNumber) {
-			throw new IndexOutOfBoundsException();
-		}
-	}
+  private void valid(int row, int col) {
+    if (row < 1 || row > this.sideNumber || col < 1 || col > this.sideNumber) {
+      throw new IndexOutOfBoundsException();
+    }
+  }
 
-	public void open(int row, int col) { // open site (row, col) if it is not
-											// open already
-		valid(row, col);
-		row = row - 1;
-		col = col - 1;
-		if (siteStatus[row * this.sideNumber + col] != 1) {
-			siteStatus[row * this.sideNumber + col] = 1;
-		}
-		if (row > 0 && siteStatus[(row - 1) * this.sideNumber + col] == 1) {
-			grid.union((row) * this.sideNumber + col, (row - 1) * this.sideNumber + col);
-		}
-		if (row < this.sideNumber - 1 && siteStatus[(row + 1) * this.sideNumber + col] == 1) {
-			grid.union((row) * this.sideNumber + col, (row + 1) * this.sideNumber + col);
-		}
-		if (col > 0 && siteStatus[(row) * this.sideNumber + col - 1] == 1) {
-			grid.union((row) * this.sideNumber + col, row * this.sideNumber + col - 1);
-		}
+  public void open(int row, int col) { // open site (row, col) if it is not
 
-		if (col < this.sideNumber - 1 && siteStatus[(row) * this.sideNumber + col + 1] == 1) {
-			grid.union((row) * this.sideNumber + col, row * this.sideNumber + col + 1);
-		}
-	}
+    valid(row, col);
+    // row = row - 1;
+    // col = col - 1;
+    if (!this.isOpen(row, col)) {
+      siteStatus[row][col] = 1;
 
-	public boolean isOpen(int row, int col) { // is site (row, col) open?
+      if (row == 1) {
+        grid.union(0, (row - 1) * this.sideNumber + (col - 1) + 1);
+      }
+      if (row == this.sideNumber) {
+        grid.union(this.sideNumber * this.sideNumber + 1, (row - 1) * this.sideNumber + (col - 1) + 1);
+      }
 
-		valid(row, col);
-		row = row - 1;
-		col = col - 1;
-		if (siteStatus[row * sideNumber + col] == 1) {
-			return true;
-		}
-		return false;
-	}
+      if (row > 1) {
+        if (this.isOpen(row - 1, col)) {
+          grid.union((row - 1) * this.sideNumber + (col - 1) + 1, ((row - 1) - 1) * this.sideNumber + (col - 1) + 1);
+        }
+      }
+      if (row < this.sideNumber) {
+        if (this.isOpen(row + 1, col)) {
+          grid.union((row - 1) * this.sideNumber + (col - 1) + 1, ((row - 1) + 1) * this.sideNumber + (col - 1) + 1);
+        }
+      }
+      if (col > 1) {
+        if (this.isOpen(row, col - 1)) {
+          grid.union((row - 1) * this.sideNumber + (col - 1) + 1, (row - 1) * this.sideNumber + (col - 1));
+        }
+      }
+      if (col < this.sideNumber) {
+        if (this.isOpen(row, col + 1)) {
+          grid.union((row - 1) * this.sideNumber + (col - 1) + 1, (row - 1) * this.sideNumber + (col - 1) + 2);
+        }
+      }
+    }
 
-	public boolean isFull(int row, int col) { // is site (row, col) full?
-		valid(row, col);
-		row = row - 1;
-		col = col - 1;
-		if (siteStatus[row * sideNumber + col] == 0) {
-			return true;
-		}
-		return false;
-	}
+    // for (int i = 1; i <= this.sideNumber; i++) {
+    // if (!grid.connected(this.sideNumber * this.sideNumber + 1, (row - 1) *
+    // this.sideNumber + (col - 1) + 1)
+    // && this.isFull(this.sideNumber, i)) {
+    // grid.union(this.sideNumber * this.sideNumber + 1, (row - 1) *
+    // this.sideNumber + (col - 1) + 1);
+    // }
+    // }
 
-	public boolean percolates() { // does the system percolate?
-		return grid.connected(0, this.sideNumber * this.sideNumber + 1);
-	}
+  }
+
+  public boolean isOpen(int row, int col) { // is site (row, col) open?
+
+    valid(row, col);
+    // row = row - 1;
+    // col = col - 1;
+    if (siteStatus[row][col] == 1) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isFull(int row, int col) { // is site (row, col) full?
+
+    valid(row, col);
+    // row = row - 1;
+    // col = col - 1;
+    if (this.isOpen(row, col)) {
+      if (grid.connected((row - 1) * this.sideNumber + (col - 1) + 1, 0)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean percolates() { // does the system percolate?
+    return grid.connected(0, this.sideNumber * this.sideNumber + 1);
+  }
+
+  public static void main(String[] args) {
+    Percolation test = new Percolation(6);
+    test.open(1, 6);
+    test.open(2, 6);
+    test.open(3, 6);
+    test.open(4, 6);
+    test.open(5, 6);
+    test.open(6, 6);
+    test.open(6, 3);
+    StdOut.println(test.isFull(6, 3));
+
+  }
 
 }
--- Assignment 1/PercolationStats.java
+++ Assignment 1/PercolationStats.java
@@ -2,57 +2,61 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import java.util.Arrays;
-//import java.lang.IllegalArgumentException;
 
 public class PercolationStats {
-	// Percolation[] percol_exp;
-	private double[] p_array;
+  // Percolation[] percol_exp;
+  private double[] pArray;
 
-	public PercolationStats(int n, int trials) { // perform trials independent
-													// experiments on an n-by-n
-													// grid
-		if (n <= 0 || trials <= 0) {
-			throw new IllegalArgumentException();
-		}
-		p_array = new double[trials];
-		for (int i = 0; i < trials; i++) {
-			p_array[i] = 0;
-			Percolation percol_exp = new Percolation(n);
-			int p = 0;
-			while (percol_exp.percolates() != true) {
-				// StdRandom rand = new StdRandom();
-				int row = StdRandom.uniform(n) + 1;
-				int col = StdRandom.uniform(n) + 1;
-				if (percol_exp.isFull(row, col)) {
-					percol_exp.open(row, col);
-					p++;
-				}
-			}
-			p_array[i] = (double) p / (n * n);
-		}
-		Arrays.sort(p_array);
-	}
+  public PercolationStats(int n, int trials) { // perform trials independent
+    // experiments on an n-by-n
+    // grid
+    if (n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException();
+    }
+    pArray = new double[trials];
+    for (int i = 0; i < trials; i++) {
+      pArray[i] = 0;
+      Percolation percolExp = new Percolation(n);
+      int p = 0;
+      while (!percolExp.percolates()) {
+        // StdRandom rand = new StdRandom();
+        int row = StdRandom.uniform(n) + 1;
+        int col = StdRandom.uniform(n) + 1;
+        // StdOut.println(row);
+        // StdOut.println(col);
+        if (!percolExp.isOpen(row, col)) {
+          p++;
+        }
+        percolExp.open(row, col);
 
-	public double mean() {
-		return StdStats.mean(p_array);
-	} // sample mean of percolation threshold
+      }
+      pArray[i] = (double) p / (n * n);
+      // StdOut.println(pArray[i]);
+    }
+    Arrays.sort(pArray);
+  }
 
-	public double stddev() {
-		return StdStats.stddev(p_array);
-	} // sample standard deviation of percolation threshold
+  public double mean() {
+    return StdStats.mean(pArray);
+  } // sample mean of percolation threshold
 
-	public double confidenceLo() {
-		return (this.mean() - 1.96 * this.stddev() / Math.sqrt(p_array.length));
-	} // low endpoint of 95% confidence interval
+  public double stddev() {
+    return StdStats.stddev(pArray);
+  } // sample standard deviation of percolation threshold
 
-	public double confidenceHi() {
-		return (this.mean() + 1.96 * this.stddev() / Math.sqrt(p_array.length));
-	} // high endpoint of 95% confidence interval
+  public double confidenceLo() {
+    return (this.mean() - 1.96 * this.stddev() / Math.sqrt(pArray.length));
+  } // low endpoint of 95% confidence interval
 
-	public static void main(String[] args) {
-		PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-		StdOut.println("mean                    = " + percolationStats.mean());
-		StdOut.println("stddev                  = " + percolationStats.stddev());
-		StdOut.println("95% confidence interval = " + percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi());
-	} // test client (described below)
+  public double confidenceHi() {
+    return (this.mean() + 1.96 * this.stddev() / Math.sqrt(pArray.length));
+  } // high endpoint of 95% confidence interval
+
+  public static void main(String[] args) {
+    PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+    StdOut.println("mean                    = " + percolationStats.mean());
+    StdOut.println("stddev                  = " + percolationStats.stddev());
+    StdOut.println(
+        "95% confidence interval = " + percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi());
+  } // test client (described below)
 }
