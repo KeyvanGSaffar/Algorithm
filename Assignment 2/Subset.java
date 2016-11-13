ab2898c022c60a2093029e26b4463ecb4077704d
Keyvan GSaffar <gharouni@ualberta.ca>
2016-11-13 16:59:37 -0700

    grade 92/100

--- Assignment 2/Subset.java
+++ Assignment 2/Subset.java
@@ -4,10 +4,10 @@ import edu.princeton.cs.algs4.StdOut;
 public class Subset {
   public static void main(String[] args) {
     int k = Integer.parseInt(args[0]);
-    RandomizedQueue<Integer> randQueue = new RandomizedQueue<Integer>();
+    RandomizedQueue<String> randQueue = new RandomizedQueue<String>();
 
     for (int i = 0; i < k; i++)
-      randQueue.enqueue(Integer.parseInt(StdIn.readString()));
+      randQueue.enqueue(StdIn.readString());
 
     for (int i = 0; i < k; i++)
       StdOut.println(randQueue.dequeue());
