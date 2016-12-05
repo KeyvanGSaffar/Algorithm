91e98f5fd58535f746f2c79c218fea4d47ef802d
Keyvan GSaffar <gharouni@uaberta.ca>
2016-12-04 17:36:56 -0700

    SearchNode classed added

---
+++ Assignment 4/SearchNode.java
@@ -0,0 +1,40 @@
+import java.util.Comparator;
+
+public class SearchNode {
+  public final Board board; 
+  public final int moves;
+  public final SearchNode searchN;
+  public static final Comparator<SearchNode> HamPriority = new ByHam();
+  public static final Comparator<SearchNode> ManhPriority = new ByManh();
+  public SearchNode(Board brd, int mv, SearchNode sN){
+    board = brd;
+    moves = mv;
+    searchN = sN;
+  }
+  
+  public static class ByHam implements Comparator<SearchNode> {
+
+    @Override
+    public int compare(SearchNode o1, SearchNode o2) {
+      // TODO Auto-generated method stub
+      int w1 = o1.board.hamming() + o1.moves;
+      int w2 = o2.board.hamming() + o2.moves;
+      return w1 - w2;
+    }
+    
+  }
+  
+  public static class ByManh implements Comparator<SearchNode> {
+
+    @Override
+    public int compare(SearchNode o1, SearchNode o2) {
+      // TODO Auto-generated method stub
+      int w1 = o1.board.manhattan() + o1.moves;
+      int w2 = o2.board.manhattan() + o2.moves;
+      return w1 - w2;
+    }
+    
+  }
+
+
+}
